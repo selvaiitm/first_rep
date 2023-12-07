9931817e29e965acf00ade652d645c08950374a9
selvaiitm <96175511+selvaiitm@users.noreply.github.com>
2023-12-07 21:50:18 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -5,8 +5,8 @@ st.title("adding two numbers")
 def addition(a,b):
   product = a+b
   return product
-a = st.number_input('first:')
-b = st.number_input('second:')
+a = st.number_input('a:')
+b = st.number_input('b:')
 
 
 st.write(f"The product of {a} and {b} is: {product}")
