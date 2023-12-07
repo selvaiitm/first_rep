d464df1f6cee2f3232177f9d11cac258fa04ee67
selvaiitm <96175511+selvaiitm@users.noreply.github.com>
2023-12-07 21:43:31 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.title('Hai welcome to my first streamlit app - Tamilselvan.N')
 
 def multiplication(a,b):
-  return a,b
-
+  product = a*b
+  return product
 a = st.number_input('first:')
 b = st.number_input('second:')
 
-multiplication(a,b)
+ st.write(f"The product of {a} and {b} is: {product}")
