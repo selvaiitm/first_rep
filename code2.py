ad468a93fec2b281422769f9f16a731edd7aed5d
selvaiitm <96175511+selvaiitm@users.noreply.github.com>
2023-12-07 21:37:18 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -5,6 +5,6 @@ st.title('Hai welcome to my first streamlit app - Tamilselvan.N')
 def multiplication(a,b):
   return a,b
 
-a = st.number_input('first':)
-b = st.number_input('second':)
+a = st.number_input('first:')
+b = st.number_input('second:')
 
