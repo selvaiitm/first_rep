11e61a504e0cc4cf78d54d94e5507a07637e3b9f
selvaiitm <96175511+selvaiitm@users.noreply.github.com>
2023-12-07 22:02:24 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -4,8 +4,11 @@ st.title('Hai welcome to my first streamlit app - Tamilselvan.N')
 st.title("Multiplying two numbers")
 def multiplication(a,b):
   return a*b
-a = st.number_input('a:')
-b = st.number_input('b:')
+a = st.number_input('Enter the first number:')
+b = st.number_input('Enter the second number:')
 
 product = multiplication(a,b)
+st.write(f'The first number is {a}')
+st.write(f'The first number is {b}')
+
 st.write(f"The product of {a} and {b} is: {product}")
